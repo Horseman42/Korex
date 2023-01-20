47d2f10e7e33d6c0bfdfeb036fe357f109282180
unknown <babalolaridwan4real@gmail.com>
2023-01-20 13:15:40 -0800

    update

--- client/script.js
+++ client/script.js
@@ -79,11 +79,13 @@ const handleSubmit = async (e)=> {
 
   const messageDiv = document.getElementById(uniqueId);
 
+  console.log(messageDiv)
+
   loader(messageDiv);
 
   //fectch data from server
 
-  const response = await fetch('https://korex-ai.onrender.com', {
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -109,7 +111,7 @@ const handleSubmit = async (e)=> {
 
     messageDiv.innerHTML = "Something went wrong";
 
-    alert(err);
+    console.log(err);
   }
 }
 
